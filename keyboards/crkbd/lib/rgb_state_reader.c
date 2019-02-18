727be1968d3350db2cdecb739e50c40b50e7ef6a
Derek Daniels <dbd@burrow>
2019-02-18 02:16:04 -0500

    Add personal keymap and update rgb state reader

--- keyboards/crkbd/lib/rgb_state_reader.c
+++ keyboards/crkbd/lib/rgb_state_reader.c
@@ -8,7 +8,7 @@ char rbf_info_str[24];
 const char *read_rgb_info(void) {
 
   snprintf(rbf_info_str, sizeof(rbf_info_str), "%s %2d h%3d s%3d v%3d",
-    rgblight_config.enable ? "on" : "- ", rgblight_config.mode,
+    rgblight_config.enable ? "on" : "off", rgblight_config.mode,
     rgblight_config.hue, rgblight_config.sat, rgblight_config.val);
   return rbf_info_str;
 }
